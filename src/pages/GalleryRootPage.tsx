e5dc8b37033c0d6e88b26cc7efa079653ac6cdb5
Daniel Xie <danielxie@Daniels-MacBook-Pro.local>
2025-05-15 10:47:00 +0100

    more photos

--- src/pages/GalleryRootPage.tsx
+++ src/pages/GalleryRootPage.tsx
@@ -6,7 +6,7 @@ const photos: Photo[] = [
       path: `images/sikkim/sikkim1.JPEG`,
       id: "sikkim",
       title: "Sikkim",
-      caption: "Dingggggggggg",
+      caption: "Into the Himalayas",
       link: 'gallery/sikkim',
     },
     {
@@ -15,6 +15,27 @@ const photos: Photo[] = [
       title: "Nagaland",
       caption: "Two heads are better than one",
       link: 'gallery/nagaland',
+    },
+    {
+      path: `images/assam/IMG_0593.JPEG`,
+      id: "assam",
+      title: "Assam",
+      caption: "Where the Brahmaputra Breathes Life",
+      link: 'gallery/assam',
+    },
+    {
+      path: `images/darjeeling/IMG_9359.JPEG`,
+      id: "darjeeling",
+      title: "Darjeeling",
+      caption: "Time for tea",
+      link: 'gallery/darjeeling',
+    },
+    {
+      path: `images/meghalaya/IMG_4813.JPEG`,
+      id: "meghalaya",
+      title: "Meghalaya",
+      caption: "Abode of clouds",
+      link: 'gallery/meghalaya',
     },
   ];
 
