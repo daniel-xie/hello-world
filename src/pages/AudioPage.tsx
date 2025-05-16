12507fa4f163c6c13388c6a38126c98c5fdc64e4
Daniel Xie <danielxie@Daniels-MacBook-Pro.local>
2025-05-16 18:31:29 +0100

    center audio player

--- src/pages/AudioPage.tsx
+++ src/pages/AudioPage.tsx
@@ -3,11 +3,13 @@ import AudioPlayer from "@/components/layout/AudioPlayer";
 export const AudioPage = () => {
   return (
     <>
+    <div className="flex align-middle justify-center">
       <AudioPlayer
       title={"kiyomizu"}
       artist={"Daniel Xie"}
       src={`${import.meta.env.BASE_URL}sounds/kiyomizu.mp3`}
       albumArt={`${import.meta.env.BASE_URL}sounds/kiyomizu.png`} />
+      </div>
     </>
   );
 };
